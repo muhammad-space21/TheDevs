28e8694ef32fef5c3d4f8c8966ed020e0ffab386
unknown <daler@gmail.com>
2020-07-26 12:48:28 +0500

    configured the Redux Store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import App from './App';
+
+import store from './redux/store/store';
 
 ReactDOM.render(
   <Router>
-    <React.StrictMode>
+    <Provider store={store}>
       <App/>
-    </React.StrictMode>
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
