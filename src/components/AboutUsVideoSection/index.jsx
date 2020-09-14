3eb4be22e5ac4283f5d96bc5c79b6eef339fb528
Daler <daler@gmail.com>
2020-09-14 22:35:56 +0500

    using Youtube package to fix iframe bug

--- src/components/AboutUsVideoSection/index.jsx
+++ src/components/AboutUsVideoSection/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
+// import ReactPlayer from 'react-player';
+import YouTube from 'react-youtube';
 
 import {
   Container,
@@ -8,21 +9,28 @@ import {
 
 import './styles.css';
 
-import Spinner from '../Spinner';
-
 const AboutUsVideoSection = () => {
+  const opts = {
+    height: '100%',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      rel: 0
+    },
+  };
+
+  const onReady = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
 
   return (
     <Container>
       <Heading>About<span>TheDevs</span></Heading>
-      {/* <div className='player-wrapper'> */}
-        <ReactPlayer
-          className='react-player'
-          url='http://www.youtube.com/watch?v=0fv4D9t3tCI'
-          width='100%'
-          height='100%'
-        />
-      {/* </div> */}
+      <div className='player-wrapper'>
+        <YouTube className="react-player" videoId="0fv4D9t3tCI" opts={opts} onReady={onReady} />
+      </div>
     </Container>
   )
 };
