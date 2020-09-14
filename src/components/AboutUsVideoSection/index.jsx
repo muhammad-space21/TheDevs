d35f7dae13d9326725b8e832acfbd7be85e44fed
Daler <daler@gmail.com>
2020-09-14 18:02:10 +0500

    put our video url

--- src/components/AboutUsVideoSection/index.jsx
+++ src/components/AboutUsVideoSection/index.jsx
@@ -16,7 +16,7 @@ const AboutUsVideoSection = () => {
         <div className='player-wrapper'>
           <ReactPlayer
             className='react-player'
-            url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+            url='https://www.youtube.com/watch?v=0fv4D9t3tCI'
             width='100%'
             height='100%'
             controls={true}
