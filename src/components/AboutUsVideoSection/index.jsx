eec73aea50342e532a5a2ef9d72a5f52dd891384
Daler <daler@gmail.com>
2020-09-14 22:23:46 +0500

    commented the reactPlayer wrapper

--- src/components/AboutUsVideoSection/index.jsx
+++ src/components/AboutUsVideoSection/index.jsx
@@ -15,14 +15,14 @@ const AboutUsVideoSection = () => {
   return (
     <Container>
       <Heading>About<span>TheDevs</span></Heading>
-      <div className='player-wrapper'>
+      {/* <div className='player-wrapper'> */}
         <ReactPlayer
           className='react-player'
           url='http://www.youtube.com/watch?v=0fv4D9t3tCI'
           width='100%'
           height='100%'
         />
-      </div>
+      {/* </div> */}
     </Container>
   )
 };
