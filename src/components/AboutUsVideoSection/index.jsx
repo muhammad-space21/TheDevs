91d1eec168ee276d64fffc197b13460b0ff362f0
Daler <daler@gmail.com>
2020-09-14 21:53:01 +0500

    changed the https to http

--- src/components/AboutUsVideoSection/index.jsx
+++ src/components/AboutUsVideoSection/index.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 
 import {
-  Container, 
+  Container,
   Heading
 } from './styles';
 
 import './styles.css';
 
 const AboutUsVideoSection = () => {
-  
   return (
-      <Container>
-        <Heading>About<span>TheDevs</span></Heading>
-        <div className='player-wrapper'>
+    <Container>
+      <Heading>About<span>TheDevs</span></Heading>
+      <div className='player-wrapper'>
         <ReactPlayer
           className='react-player'
-          url='https://www.youtube.com/watch?v=0fv4D9t3tCI'
+          url='http://www.youtube.com/watch?v=0fv4D9t3tCI'
           width='100%'
           height='100%'
+          origin='http://localhost:3000'
         />
       </div>
-      </Container>
+    </Container>
   )
 };
 
